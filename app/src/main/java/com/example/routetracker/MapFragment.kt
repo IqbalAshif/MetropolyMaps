6203d58d77f70e0d8f6cee1040d9121188c7ec3a
Cyroxin <Cyroxin@github.com>
2021-10-01 18:59:38 +0300

    Add points of interest

--- app/src/main/java/com/example/routetracker/MapFragment.kt
+++ app/src/main/java/com/example/routetracker/MapFragment.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
@@ -25,13 +26,20 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import org.osmdroid.bonuspack.location.NominatimPOIProvider
+import org.osmdroid.bonuspack.location.POI
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Overlay
 import org.osmdroid.views.overlay.Polyline
+import java.lang.NullPointerException
 import kotlin.math.roundToInt
+import androidx.core.graphics.scale
+import org.osmdroid.events.MapAdapter
+import org.osmdroid.events.ZoomEvent
 
 
 class MapFragment : Fragment(), LocationListener {
@@ -45,6 +53,8 @@ class MapFragment : Fragment(), LocationListener {
     private lateinit var marker: Marker
     private lateinit var path: Polyline
 
+    private var pois: MutableList<POI> = mutableListOf()
+
     private lateinit var toggle: FloatingActionButton
     private lateinit var info: FloatingActionButton
 
@@ -56,6 +66,9 @@ class MapFragment : Fragment(), LocationListener {
         fun newInstance() = MapFragment()
     }
 
+    val userAgent = BuildConfig.APPLICATION_ID + "/" + BuildConfig.VERSION_NAME
+
+
     /* UI */
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -65,16 +78,17 @@ class MapFragment : Fragment(), LocationListener {
         val view = inflater.inflate(R.layout.fragment_map, container, false)
         super.onCreate(savedInstanceState)
 
-       Configuration.getInstance()
+        Configuration.getInstance()
             .load(context, PreferenceManager.getDefaultSharedPreferences(context))
 
         // Stepcounter
         stepCount = view.findViewById<TextView>(R.id.steps)
         stepSensor = StepSensor(context)
-        stepSensor.onStopped = { stepCount.text = ""}
+        stepSensor.onStopped = { stepCount.text = "" }
         stepSensor.onTriggered = {
-            if(stepCount.tag != null && toggle.tag == true)
-                stepCount.text = (stepCount.text.toString().ifEmpty { "0" }.ifBlank { "0" }.toFloat() + it.values[0] - (stepCount.tag as Float)).roundToInt().toString()
+            if (stepCount.tag != null && toggle.tag == true)
+                stepCount.text = (stepCount.text.toString().ifEmpty { "0" }.ifBlank { "0" }
+                    .toFloat() + it.values[0] - (stepCount.tag as Float)).roundToInt().toString()
 
             stepCount.tag = it.values[0]
         }
@@ -87,13 +101,23 @@ class MapFragment : Fragment(), LocationListener {
         map.isTilesScaledToDpi = true
         map.setMultiTouchControls(true)
         map.controller.setZoom(3.0)
+        map.addMapListener(object : MapAdapter() {
+            override fun onZoom(event: ZoomEvent?): Boolean {
+                if (event != null && pois.isNotEmpty())
+                    if (event.zoomLevel >= 18)
+                        refreshPointsOfInterest(true) // Use tile icons when they appear
+                    else
+                        refreshPointsOfInterest(false)
+                return super.onZoom(event)
+            }
+        })
         createOverlays()
 
         // Gps Fab
         toggle = view.findViewById<FloatingActionButton>(R.id.toggle)
         toggle.tag = false // Recording?
         toggle.backgroundTintList = ColorStateList.valueOf(Color.GREEN + Color.GREEN * 40 / 100)
-        toggle.setOnClickListener{
+        toggle.setOnClickListener {
             if (toggle.tag == false && requestLocationPermissions(requireActivity()))
                 enableGps() // Start recording
             else
@@ -103,10 +127,13 @@ class MapFragment : Fragment(), LocationListener {
         // Info Fab
         info = view.findViewById<FloatingActionButton>(R.id.info)
         info.setOnClickListener {
+            /*
             parentFragmentManager.beginTransaction().hide(this)
                 .add(R.id.fragmentContainerView, DashboardFragment.newInstance())
                 .addToBackStack("")
                 .commit()
+             */
+            fetchPointsOfInterest()
         }
 
         // Animations
@@ -119,7 +146,7 @@ class MapFragment : Fragment(), LocationListener {
         //startActivity(mapIntent)
 
         // Intent
-        if(activity != null && requireActivity().intent.data != null) {
+        if (activity != null && requireActivity().intent.data != null) {
             map.controller.setCenter(parseLocation(requireActivity().intent.data.toString()))
             map.controller.setZoom(18.0)
         }
@@ -158,6 +185,47 @@ class MapFragment : Fragment(), LocationListener {
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         marker.alpha = 0f
         map.overlays.add(marker)
+    }
+
+    private fun fetchPointsOfInterest() {
+        CoroutineScope(Dispatchers.IO).launch {
+            val poiProvider = NominatimPOIProvider(userAgent)
+            try {
+                pois = poiProvider.getPOIInside(map.boundingBox, "Restaurant", 30)
+                Log.e("NewPois", pois.size.toString())
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    refreshPointsOfInterest()
+                }
+            } catch (exp: NullPointerException) { // Network Error
+                Log.e("Network Error", "Failed to fetch points of interest")
+            }
+        }
+    }
+
+    private fun refreshPointsOfInterest(invisible: Boolean = false) {
+
+        CoroutineScope(Dispatchers.Unconfined).launch {
+            val overlays: MutableList<Overlay> = mutableListOf()
+            map.overlays.removeAll { it != marker } // Remove old overlays if any exist
+
+            Log.e("PoisRefresh", pois.size.toString())
+
+            pois.forEach {
+                marker = Marker(map)
+                marker.icon = BitmapDrawable(resources, it.mThumbnail.scale(100, 100))
+                marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+                marker.position = it.mLocation
+                marker.title = it.mDescription.takeWhile { it != ',' }
+                if (invisible) marker.alpha = 0f
+                overlays.add(marker)
+            }
+
+            CoroutineScope(Dispatchers.Main).launch {
+                map.overlays.addAll(overlays)
+                map.invalidate()
+            }
+        }
     }
 
     /* Location */
