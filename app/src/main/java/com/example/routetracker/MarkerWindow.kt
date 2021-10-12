070fac328b13cc07369f01891d69fd9b802f3b33
Cyroxin <Cyroxin@github.com>
2021-10-12 19:59:39 +0300

    Improve routing and usability

--- app/src/main/java/com/example/routetracker/MarkerWindow.kt
+++ app/src/main/java/com/example/routetracker/MarkerWindow.kt
@@ -31,77 +31,16 @@ class MarkerWindow(val context: Context, mapView: MapView, val mapFragment: MapF
 
     override fun onOpen(item: Any?) {
         closeAllInfoWindowsOn(mapView)
-        val routeButton = view.findViewById<Button>(R.id.btRoute)
+
         //clicking route button
-        routeButton.setOnClickListener {
-            onRoute()
+        view.findViewById<Button>(R.id.btRoute).setOnClickListener { onRoute() }
 
-        }
-
-        mView.setOnClickListener {
-            close()
-        }
+        mView.setOnClickListener { close() }
     }
 
     //setting the title of the place to the textview
     fun seTitle(title: String) {
         view.findViewById<TextView>(R.id.tvTitle).text = title
-    }
-
-
-    private fun addMarker(point: GeoPoint, title: String) {
-        val startMarker = Marker(mapView)
-        startMarker.position = point
-        startMarker.title = title
-        startMarker.icon = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_position)
-        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-        if (mapFragment.destinationMarker != null) {
-            mapView.overlays.remove(mapFragment.destinationMarker)
-        }
-        mapFragment.destinationMarker = startMarker
-        mapView.overlays?.add(startMarker)
-        mapView.invalidate()
-
-    }
-
-    fun addingRouteLocations(startPoint: GeoPoint, endPoint: GeoPoint) {
-        val roadManager = OSRMRoadManager(context, "MY_USER_AGENT")
-
-        val routePoints = ArrayList<GeoPoint>()
-        routePoints.add(startPoint)
-        routePoints.add(endPoint)
-        CoroutineScope(Dispatchers.IO).launch {
-            gettingRoad(roadManager, routePoints)
-            mapView.invalidate()
-        }
-        addMarker(endPoint, "Destination")
-    }
-
-    private fun gettingRoad(roadManager: OSRMRoadManager, waypoints: ArrayList<GeoPoint>) {
-        // Retrieving road
-        roadManager.setMean(OSRMRoadManager.MEAN_BY_FOOT)
-        val road = roadManager.getRoad(waypoints)
-        if (mapFragment.route != null) {
-            mapView.overlays.remove(mapFragment.route)
-        }
-        val roadOverlay = RoadManager.buildRoadOverlay(road, 0xAA0000FF.toInt(), 10.5F)
-        mapFragment.route = roadOverlay
-        mapView.overlays.add(roadOverlay)
-        //Marker at each node
-        val nodeIcon = AppCompatResources.getDrawable(context, R.drawable.ic_baseline_stop_24)
-        for (i in 0 until road.mNodes.size) {
-            val node = road.mNodes[i]
-            val nodeMarker = Marker(mapView)
-            nodeMarker.position = node.mLocation
-            nodeMarker.icon = nodeIcon
-            nodeMarker.title = "Step $i"
-            mapView.overlays.add(nodeMarker)
-            nodeMarker.snippet = node.mInstructions
-            nodeMarker.subDescription =
-                Road.getLengthDurationText(mapView.context, node.mLength, node.mDuration)
-        }
-
-        mapView.invalidate()
     }
 
     override fun onClose() {
